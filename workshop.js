101d1719b9b8897b235106079fd9fa4b2bc77702
Joy <travelerjoy85@gmail.com>
2017-04-05 22:56:20 +0000

    indexOf done

--- workshop.js
+++ workshop.js
@@ -59,7 +59,15 @@ function some(predicate, theArray) {
 }
 
 function indexOf(item, theArray) {
-
+  if(theArray.length === 0){
+    return -1;
+  }
+  for(var i = 0; i < theArray.length; i++){
+    if(item === theArray[i]){
+      return i;
+    }
+  }
+  return -1;
 }
 
 function findIndex(predicate, theArray) {

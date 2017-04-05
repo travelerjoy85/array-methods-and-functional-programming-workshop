f8d43742e7ba5728d4004bf64d9053f8a1f19669
Joy <travelerjoy85@gmail.com>
2017-04-05 23:02:19 +0000

    findIndex done

--- workshop.js
+++ workshop.js
@@ -71,7 +71,12 @@ function indexOf(item, theArray) {
 }
 
 function findIndex(predicate, theArray) {
-
+  for(var i = 0; i < theArray.length; i++){
+    if(predicate(theArray[i])){
+      return i;
+    }
+  }
+  return -1;
 }
 
 function first(n, theArray) {
